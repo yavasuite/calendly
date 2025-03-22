07c110d84360d35b8be70d6683b9f8b365d690fc
The Executor <yavasuite@gmail.com>
2025-03-22 19:41:34 -0400

    register route

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,6 +1,8 @@
 from .auth import auth_bp
 from .main import main
+from .meeting import meeting_bp
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(main)
+    app.register_blueprint(meeting_bp)
